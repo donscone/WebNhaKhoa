6251a50c7a4172fb82dff99927db8e2b3c2a824b
donscone <duycong2580@gmail.com>
2024-04-05 19:56:14 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NhaKhoaQuangVu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
